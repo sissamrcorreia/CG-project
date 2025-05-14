f07204f955b77e1b1d5e5fa790363606ec7171db
Francisca Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2025-05-14 17:51:59 +0100

    Add trailer class and integrate into scene creation

--- TODO.md
+++ TODO.md
@@ -2,11 +2,12 @@
 * [ ] Câmaras fixas de projeção ortogonal com vistas frontal, lateral e de topo sobre a cena ‘1’ (frontal), ‘2’ (lateral), ‘3’ (topo) e ‘4’ (perspetiva)
 * [ ] Fazer o robô apenas com cilindros, cubos e cones
 * [ ] Reboque com cilindros e cubos
-* [ ] Teclado:* ‘Q(q)’ e ‘A(a)’ para controlar o ângulo θ1 que roda o eixo de revolução dos pés;
+* [ ] Teclado:
+  * [ ]‘Q(q)’ e ‘A(a)’ para controlar o ângulo θ1 que roda o eixo de revolução dos pés;
 
   * [ ] ‘W(w)’ e ‘S(s)’ para controlar o ângulo θ2 que roda o eixo de revolução da cintura;
   * [ ] ‘E(e)’ e ‘D(d)’ para controlar o deslocamento δ1 que translaciona os membros superiores medial e lateralmente;
-* [ ] ‘R(r)’ e ‘F(f)’ para controlar o ângulo θ3 que roda o eixo de revolução da cabeça.
+  * [ ] ‘R(r)’ e ‘F(f)’ para controlar o ângulo θ3 que roda o eixo de revolução da cabeça.
 * [ ] Permitir ao utilizador deslocar o reboque pelo plano invisível com o teclado utilizando as teclas das setas para o reposicionar segundo os eixos globais dos X e Y, respetivamente. O referencial do reboque deve estar alinhado com o referencial do robô-camião
 * [ ] A representação visual dos objetos da cena deve alternar entre modelo de arames e sólida usando a tecla ‘7’
 * [ ] AABBs para detetar a colisão
--- js/main-script.js
+++ js/main-script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Trailer } from "./trailer.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 // import { VRButton } from "three/addons/webxr/VRButton.js";
 // import * as Stats from "three/addons/libs/stats.module.js";
 // import { GUI } from "three/addons/libs/lil-gui.module.min.js";
@@ -19,13 +20,23 @@ let renderer, scene;
 function createScene() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xffffff); // Set background color
+
+  // trailer
+  const trailer = new Trailer();
+  trailer.position.set(0, 0, 0);
+  scene.add(trailer);
+
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
 const camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
-camera.position.set(0, 5, 10); // Set camera position (x, y, z)
+camera.position.set(0, 5, 50); // Set camera position (x, y, z)
+
+
+// TODO: remove this line
+let controls;
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -72,6 +83,17 @@ function init() {
   createScene();
   render()
 
+  // TODO: remove this line
+  controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+  controls.minDistance = 10;
+controls.maxDistance = 200;
+controls.maxPolarAngle = Math.PI / 2; // 90 degrees
+controls.minPolarAngle = 0;          // top view
+
+
+
   window.addEventListener("resize", onResize);
 }
 
@@ -79,6 +101,9 @@ function init() {
 /* ANIMATION CYCLE */
 /////////////////////
 function animate() {
+  // TODO: remove after this line
+  controls.update(); // only required if controls.enableDamping = true, or autoRotate is true
+  requestAnimationFrame(animate);
   render();
 }
 

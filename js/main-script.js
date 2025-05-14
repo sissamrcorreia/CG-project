c3a041bc9ed1e15b613306388aef69ff81d61ee7
Francisca Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2025-05-14 16:12:23 +0100

    Improve initial JavaScript files for project setup

--- js/main-script.js
+++ js/main-script.js
@@ -1,21 +1,30 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { VRButton } from "three/addons/webxr/VRButton.js";
-import * as Stats from "three/addons/libs/stats.module.js";
-import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import { VRButton } from "three/addons/webxr/VRButton.js";
+// import * as Stats from "three/addons/libs/stats.module.js";
+// import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
+const HEIGHT = window.innerHeight;
+const WIDTH = window.innerWidth;
+const CLOCK = new THREE.Clock();
+
+let renderer, scene;
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createScene() {}
+function createScene() {
+  scene = new THREE.Scene();
+}
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
+const camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
+camera.position.set(0, 5, 10); // Set camera position (x, y, z)
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -43,17 +52,31 @@ function update() {}
 /////////////
 /* DISPLAY */
 /////////////
-function render() {}
+function render() {
+  renderer.render(scene, camera);
+}
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
 ////////////////////////////////
-function init() {}
+function init() {
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    //   alpha: true, // Enable transparency for the background // TODO: Check if it is transparent or not
+  });
+
+  renderer.setSize(WIDTH, HEIGHT); // TODO: Resize if window is resized
+  document.body.appendChild(renderer.domElement);
+
+  createScene();
+}
 
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
-function animate() {}
+function animate() {
+  render();
+}
 
 ////////////////////////////
 /* RESIZE WINDOW CALLBACK */
